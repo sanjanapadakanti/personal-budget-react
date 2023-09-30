49b002abaf549247b8d106b0cae6be53c7c16e25
Sanjana Padakanti <spadakan@uncc.edu>
2023-09-30 00:17:18 -0400

    changed initial code to react

---
+++ src/AboutPage/AboutPage.js
@@ -0,0 +1,12 @@
+import react from "react";
+
+function AboutPage() {
+    return (
+      <div>
+         AboutPage
+      </div>
+    );
+  }
+
+export default AboutPage;
+  
---
+++ src/Footer/Footer.js
@@ -0,0 +1,14 @@
+import react from "react";
+
+function Footer() {
+    return (
+        <footer className="bottom">
+        <div className="center">
+            All rights reserved &copy; Sanjana Padakanti
+        </div>
+    </footer>
+    );
+  }
+
+export default Footer;
+  
